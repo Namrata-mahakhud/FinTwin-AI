0a568b3bb3866f5574857febe8be2c0c68479445
w3schools-test <test@w3schools.com>
2026-06-09 00:00:06 +0530

    Changes in vite.mjs

--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -36,9 +36,13 @@ export default defineConfig({
     sourcemap: true,
     rollupOptions: {
       output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'],
-          charts: ['recharts'],
+        manualChunks: (id) => {
+        if (id.includes('node_modules/react') || id.includes('node_modules/react-dom') || id.includes('node_modules/react-router-dom')) {
+          return 'vendor';
+        }
+        if (id.includes('node_modules/recharts')) {
+          return 'charts';
+        }
         },
       },
     },
